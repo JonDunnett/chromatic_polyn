0dcc739a2c3b0542ef1a819021d682856d18466f
JonDunnett <dunnett_jonathan@wheatoncollege.edu>
2017-11-16 13:15:44 -0500

    fixed

--- polyn.cpp
+++ polyn.cpp
@@ -1,4 +1,6 @@
 //polyn.cpp
+#include <stdlib.h>
+using namespace std;
 
 class polyn {
 // a polynomial stored as an integer array
@@ -19,6 +21,7 @@ public:
 	operator*(int);            // scalar multiplicaiton of polynomial
 };
 
+
 polyn::polyn() {
 // default ctor
 // PRE : 
@@ -31,30 +34,25 @@ polyn::polyn(int degree) {
 // PRE : an existing integer >=0
 // POST: a polyn object will be created that can hold 
 //       polynomials of degree == input parameter
-	this->coefs = new int[degree];
+	this->coefs = (int*)malloc(sizeof(int) * degree);
 	this->dg = degree;
 }
 
-polyn::polyn(const polyn& original) {
-// copy ctor
-// PRE :
-// POST:
-	this->dg = original.degree();
-	this->coefs = new int[original.degree];
+
+polyn::polyn(const polyn* original) {
+	this->dg = original->degree();
+	this->coefs = (int*) malloc(sizeof(int) * original->degree());
 	for(short i=0;i<this->dg;i++){
-		this->coefs[i] = other.term(i);
+		this->coefs[i] = original->term(i);
 	}
 }
 
+/*
 int polyn::degree () {
-// PRE :
-// POST:
 	return this->dg;
 }
 
 int polyn::term (int pos) {
-// PRE :
-// POST:
 	if (pos < degree) {
 		return coefs[pos];
 	} else {
@@ -62,9 +60,8 @@ int polyn::term (int pos) {
 	}
 }
 
+/*
 void polyn::set_term(int c, int pos) {
-// PRE :
-// POST:
 	if (this->dg > pos) {
 		this->coefs[pos] = c;
 	} else {
@@ -79,10 +76,8 @@ void polyn::set_term(int c, int pos) {
 	}
 }
 
-
-polyn polyn::operator+(const polyn& other) {
-// PRE :
-// POST:
+/*
+polyn& polyn::operator+(const polyn& other) {
 	int x = (this->degree()>other.degree)?this->degree():other.degree();
 	polyn R = new polyn(x);
 	for(short i=0;i<x;i++){
@@ -90,10 +85,9 @@ polyn polyn::operator+(const polyn& other) {
 	}
 	return R;
 }
-	
-polyn polyn::operator-(const polyn& other) {
-// PRE :
-// POST:
+
+/*	
+polyn& polyn::operator-(const polyn& other) {
 	int x = (this->degree()>other.degree)?this->degree : other.degree();
 	polyn R = polyn(x);
 	for(short i=0;i<x;i++){
@@ -101,10 +95,9 @@ polyn polyn::operator-(const polyn& other) {
 	}
 	return R;
 }
-	
-polyn polyn::operator*(const polyn& other) {
-// PRE :
-// POST:
+
+/*	
+polyn& polyn::operator*(const polyn& other) {
 	polyn R = polyn(this->degree() + other.degree());
 	for(short i=0;i<this->degree();i++){
 		for(short j=0;j<other.degree;j++){
@@ -114,12 +107,16 @@ polyn polyn::operator*(const polyn& other) {
 	return R;
 }
 
-polyn polyn::operator*(int scalar) {
-// PRE :
-// POST:
+/*
+polyn& polyn::operator*(int scalar) {
 	polyn R = polyn(this);
 	for (short i=0;i<this->degree();i++) {
 		R.set_term(scalar*this->term(i),i);
 	}
 	return R;
+}
+*/
+
+int main () {
+	return 0;
 }
